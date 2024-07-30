1a7fe6bf0585b4eb11ea135ad3bccb7298c9fc58
paivaanajulia <116007247+paivaanajulia@users.noreply.github.com>
2024-07-30 09:07:06 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,59 @@ const caixaPerguntas = document.querySelector('.caixa-perguntas');
 const caixaAlternativas = document.querySelector('.caixa-alternativas');
 const caixaResultado = document.querySelector('.caixa-resultado');
 const textoResultado = document.querySelector('.texto-resultado');
+
+ const perguntas = [
+    {
+        enunciado: "qual pais tem o maior indice de pobreza?"
+        alternativas: [
+        {
+            texto:"Africa"
+            afirmação:"Afirmação da alternativa 1"
+        },
+
+        { texto:"Somália"
+          afirmação: "afirmação da alternativa 2"
+       },
+
+       {  texto:"Suiça"
+        afirmação:"afirmação a alternativa 3"
+      },
+
+       { texto:"Cuba"
+         afirmação:"afirmação da alternativa 4"
+            },
+             ]
+      {,
+
+      }
+        enunciado: "qual o estado brasileiro mais racista?",
+        alternativas: [,
+        {
+            texto:"São Paulo",
+            afirmação:""
+           },
+
+           { texto:"Paraná",
+             afirmação:""
+            },
+
+            { texto:"Rio Grande do Sul",
+              afirmação:""
+            },
+
+            { texto:"Santa Catarina",
+              afirmação:""
+            },
+        ]
+  
+        {
+
+         enunciado: "qual dos seguintes fatores e mais frequentemente citado como uma causa primaria da desigualdade social em sociedade modernas?"
+         alternativas: [
+            {
+                texto:"Distribuição desigualdade de requeza e renda",
+                afirmação:""
+               },
+            }
+         ]
+        }
